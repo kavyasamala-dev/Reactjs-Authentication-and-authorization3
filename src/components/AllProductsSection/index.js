6c738a2b16863c04bd070db5b9dd9ff537a9941a
Kavya Samala <kavyasamala9@gmail.com>
2023-03-02 10:53:41 +0000

    authentication-and-authorization

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,69 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import ProductCard from '../ProductCard'
+
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+
+    const response = await fetch(apiUrl, options)
+
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.products.map(eachProduct => ({
+        title: eachProduct.title,
+        brand: eachProduct.brand,
+        price: eachProduct.price,
+        id: eachProduct.id,
+        rating: eachProduct.rating,
+        imageUrl: eachProduct.image_url,
+      }))
+      this.setState({productsList: updatedData, isLoading: false})
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList, isLoading} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          <ul className="products-list">
+            {productsList.map(product => (
+              <ProductCard productData={product} key={product.id} />
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
